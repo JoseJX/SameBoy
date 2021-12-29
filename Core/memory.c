a271e87280f236c151cbc39ef7dd33895289969e
Joe Jezak <josejx@gmail.com>
2021-12-29 17:03:46 -0500

    Fix issue in prints.

--- Core/memory.c
+++ Core/memory.c
@@ -632,7 +632,7 @@ static uint8_t read_high_memory(GB_gameboy_t *gb, uint16_t addr)
             case GB_IO_DMA:
                 return gb->io_registers[addr & 0xFF];
             case GB_IO_LCDC:
-	    	GB_log(gb, "IO_LCDC Accessed for read ROM: 0x%02X PC: 0x%04X\n", gb->mbc_rom_bank, gb->pc);
+	    	GB_log(gb, "IO_LCDC Accessed for read ROM: 0x%02X PC: 0x%04X\n", (gb->pc >= 0x4000) ? gb->mbc_rom_bank : 0, gb->pc);
 	    	return reverseValue(gb->io_registers[addr & 0xFF]);
             case GB_IO_LCDC_PKT:
 	    	return reverseValue(gb->io_registers[0x40]);
@@ -1357,7 +1357,7 @@ static void write_high_memory(GB_gameboy_t *gb, uint16_t addr, uint8_t value)
 
 
             case GB_IO_LCDC:
-	    	GB_log(gb, "IO_LCDC Accessed with value to write: 0x%02X ROM: %02X PC: 0x%04X\n", value, gb->mbc_rom_bank, gb->pc);
+	    	GB_log(gb, "IO_LCDC Accessed with value to write: 0x%02X ROM: %02X PC: 0x%04X\n", value, (gb->pc >= 0x4000) ? gb->mbc_rom_bank : 0, gb->pc);
 	    case GB_IO_LCDC_PKT:
 	    	value = reverseValue(value);
                 if ((value & 0x80) && !(gb->io_registers[GB_IO_LCDC] & 0x80)) {
@@ -1602,7 +1602,7 @@ static void write_high_memory(GB_gameboy_t *gb, uint16_t addr, uint8_t value)
                     GB_apu_write(gb, addr & 0xFF, value);
                     return;
                 }
-                GB_log(gb, "Wrote %02x to %04x (HW Register)\n", value, addr);
+	    	GB_log(gb, "Wrote %02x to %04x (HW Register) ROM: 0x%02X PC: 0x%04X\n", value, addr, (gb->pc >= 0x4000) ? gb->mbc_rom_bank : 0, gb->pc);
                 return;
         }
     }
